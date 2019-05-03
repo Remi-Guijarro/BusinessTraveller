0c2eb35f148a6f246f7a67e5bf4dd0f08947bcc3
Remi-Guijarro <remi.GUIJARRO-ESPINOSA@etu.univ-amu.fr>
2019-05-03 12:15:13 +0200

    Added simple test cases for city and main method to get duration and used memory

--- src/main/java/BusinessTraveller.java
+++ src/main/java/BusinessTraveller.java
@@ -1,5 +1,27 @@
 public class BusinessTraveller {
+
+    private static double startTime;
+
+    private static void startTimer(){
+        startTime = System.nanoTime();
+    }
+
+    private static double getSecondDuration(){
+        return ((double)System.nanoTime() - startTime) / 1000000000;
+    }
+
+    private static double getMemoryUsage(){
+        return ((double) Runtime.getRuntime().totalMemory() -  Runtime.getRuntime().freeMemory() ) / 1048576;
+    }
+
     public static void main(String[] args){
-        // Plop
+        startTimer();
+        try {
+            Thread.sleep(345);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(" Duration : " + getSecondDuration() + " seconds");
+        System.out.println(" Memory usage : " + getMemoryUsage()+ " MB ");
     }
 }
